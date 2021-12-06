77bf1eb3bba64bcd594531beb73dceefa5716a15
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-06 14:12:40 +0100

    constructor dependency injection

--- helloworld/src/main/java/com/test/skoleni/service/ItemService.java
+++ helloworld/src/main/java/com/test/skoleni/service/ItemService.java
@@ -1,14 +1,16 @@
 package com.test.skoleni.service;
 
 import com.test.skoleni.repository.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
 
-    @Autowired
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     public String getCount() {
         return "items count = " + itemRepository.count();

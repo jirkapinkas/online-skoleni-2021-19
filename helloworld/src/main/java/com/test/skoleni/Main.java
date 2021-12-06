d14921596662aa3f9551e1f94fcfe8ce237a30d0
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-06 15:09:11 +0100

    prace s databazi

--- helloworld/src/main/java/com/test/skoleni/Main.java
+++ helloworld/src/main/java/com/test/skoleni/Main.java
@@ -1,13 +1,30 @@
 package com.test.skoleni;
 
 import com.test.skoleni.service.ItemService;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @ComponentScan
 public class Main {
+
+    @Bean
+    public HikariDataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:hsqldb:hsql://localhost/eshop");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(HikariDataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext
--- helloworld/src/main/java/com/test/skoleni/repository/ItemRepository.java
+++ helloworld/src/main/java/com/test/skoleni/repository/ItemRepository.java
@@ -1,12 +1,21 @@
 package com.test.skoleni.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ItemRepository {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public ItemRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public long count() {
-        return 123;
+        String sql = "select count(*) from item";
+        return jdbcTemplate.queryForObject(sql, Long.class);
     }
 
 }

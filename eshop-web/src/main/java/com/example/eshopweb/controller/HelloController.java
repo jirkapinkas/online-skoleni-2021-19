1c000fe7d14b10c6db3fdc5985a04f31933981bd
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-07 11:10:32 +0100

    rest controller hello world (/message)

---
+++ eshop-web/src/main/java/com/example/eshopweb/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.eshopweb.controller;
+
+import com.example.eshopweb.pojo.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    // http://localhost:8080/hello
+    @GetMapping("/hello")
+    public Message message() {
+        return new Message("Spring Boot Rulez :-)");
+    }
+
+}
---
+++ eshop-web/src/main/java/com/example/eshopweb/pojo/Message.java
@@ -0,0 +1,23 @@
+package com.example.eshopweb.pojo;
+
+public class Message {
+
+    private String message;
+
+    // alt + insert
+
+    public Message() {
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

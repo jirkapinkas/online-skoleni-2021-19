12cbd7b3f5e20430f3c8ecf254d5419230b3810e
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-07 11:32:29 +0100

    low level pristup k servletu

--- eshop-web/src/main/java/com/example/eshopweb/controller/HelloController.java
+++ eshop-web/src/main/java/com/example/eshopweb/controller/HelloController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Locale;
+
 @RestController
 public class HelloController {
 
@@ -18,6 +24,12 @@ public class HelloController {
     @GetMapping(path = "/hello", params = "name")
     public Message message(@RequestParam String name) {
         return new Message("Hello " + name + ", Spring Boot Rulez :-)");
+    }
+
+    @GetMapping("/hello2")
+    public void message(Locale locale, HttpSession session,
+                           HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().write("low level shit");
     }
 
 }

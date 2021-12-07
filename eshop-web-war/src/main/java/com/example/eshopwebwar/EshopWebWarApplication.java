ff39b3940e19e4d2310c4432119f24d4d90cc772
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-07 10:39:09 +0100

    spring boot WAR priklad

---
+++ eshop-web-war/src/main/java/com/example/eshopwebwar/EshopWebWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.eshopwebwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopWebWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopWebWarApplication.class, args);
+    }
+
+}
---
+++ eshop-web-war/src/test/java/com/example/eshopwebwar/EshopWebWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eshopwebwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopWebWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

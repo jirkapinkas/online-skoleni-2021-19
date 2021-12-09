bb11771e81d2efc52dbf578e902459650b3e8393
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-09 13:22:14 +0100

    + ziskani listu zaznamu ze serveru

--- eshop-frontend/src/components/ItemList.js
+++ eshop-frontend/src/components/ItemList.js
@@ -1,18 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Item from "./Item";
 
 function ItemList() {
-    const data = [
-        {
-            id : 1,
-            name : "first",
-            price : 123
-        },
-        {
-            id : 2,
-            name : "second",
-            price : 321
-        }
-    ];
+
+    let initialData = [];
+    const [data, setData] = useState(initialData);
+  
+    const loadData = () => {
+        axios.get('http://localhost:8080/item')
+            .then(response => {
+                // console.log(response.data);
+                setData(response.data);
+            });
+    }
+    useEffect(() => {
+        loadData();
+      }, []);
+
+    // const data = [
+    //     {
+    //         id : 1,
+    //         name : "first",
+    //         price : 123
+    //     },
+    //     {
+    //         id : 2,
+    //         name : "second",
+    //         price : 321
+    //     }
+    // ];
     // data.forEach(item => console.log(item));
 
     // const someFunction = function() {

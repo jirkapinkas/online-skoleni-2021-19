0098096003ef0510dc9528091aa6ccf1db85c8d9
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-09 12:51:20 +0100

    + odblokovani CORS pri vyvoji

--- eshop-web/src/main/java/com/example/eshopweb/EshopWebApplication.java
+++ eshop-web/src/main/java/com/example/eshopweb/EshopWebApplication.java
@@ -2,9 +2,24 @@ package com.example.eshopweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EshopWebApplication {
+
+    @Profile("!test") // Tato bean se zapoji do Spring kontejneru kdyz neni aktivni profil "test" => pri vyvoji
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EshopWebApplication.class, args);

0cbcb3c57fcd37c1005e42c48f1c0a2d69b6b508
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-09 08:54:28 +0100

    + css

--- eshop-frontend/src/components/Footer.js
+++ eshop-frontend/src/components/Footer.js
@@ -1,6 +1,8 @@
+import './Footer.css'
+
 function Footer() {
     return (
-        <div>
+        <div className="Footer_footer">
             <div>(C) Skoleni Java</div>
             <div>2021 - ?</div>
         </div>

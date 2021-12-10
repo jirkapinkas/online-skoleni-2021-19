6e6b8fc64c6c0fb0b681912c4d33447f01970ef4
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-12-10 13:45:24 +0100

    + spring security custom message

--- eshop-web/src/main/java/com/example/eshopweb/SpringSecurityConfig.java
+++ eshop-web/src/main/java/com/example/eshopweb/SpringSecurityConfig.java
@@ -1,14 +1,25 @@
 package com.example.eshopweb;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +30,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
                 .antMatchers("/**")
                 .hasRole("USER")
                 .and()
@@ -26,7 +39,34 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic()
                 .and()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
+                .and()
                 .logout();
+    }
+
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return new CustomAuthenticationEntryPoint();
+    }
+
+    public static class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+        @Override
+        public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException authException) throws IOException, ServletException {
+            res.setContentType("application/json;charset=UTF-8");
+            res.setStatus(403);
+            ErrResult errResult = new ErrResult(403, "Sorry jako ...");
+            res.getWriter().write(new ObjectMapper().writeValueAsString(errResult));
+        }
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public static class ErrResult {
+        private int code;
+        private String message;
     }
 
     @Bean
